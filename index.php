8d59f4f2170d9f78f4a0cee9e6a661cb2992f752
ezequiel <zeki-san@hotmail.com>
2015-06-02 21:27:56 -0300

    Initial commit

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ function __autoload($arquivo)
 		include_once("classes/".$arquivo.".php");
 	}else if(file_exists("db/".$arquivo.".php")){
 		include_once("db/".$arquivo.".php");
-		die();
 	}
 	else{
 		throw new Exception('Unable to load class named $arquivo');
